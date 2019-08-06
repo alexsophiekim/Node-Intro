72ab08463d0f302f85da05dc0c7fcadf050c338a
Sophie Kim <270010565@bun2-206-17.local>
2019-08-07 09:36:23 +1200

    fileSystem askName() completed but still working on the askQuestions()

--- fsQuiz.js
+++ fsQuiz.js
@@ -1,5 +1,4 @@
 /*
-
 Cloud Task for 6th August
 
 Combine both the process and filesystems modules to create a new quiz.
@@ -11,7 +10,6 @@ This quiz needs to (in no particular order)
 - Output a .txt file with their name as the filename which has all their results on it
 - If someone with the same name does the quiz, we want a different file to be created rather than the other persons file be remove.
 */
-
 
 const fs = require('fs');
 const process = require('process');
@@ -25,21 +23,54 @@ const correctAnswers =[
   '16'
 ];
 
-const inputAnswer=[];
+const usersAnswer=[];
 
+let userName;
+let validUserName = false;
 
 function askName(){
-  process.stdout.write(`What is your name?\n`);
+  process.stdout.write(`Hello,\nWhat is your name?\n`);
 }
 
-process.stdin.on('data',(answer) => {
-  let inputAnswer = answer.toString().trim();
-  const name = `User name is ${inputAnswer}`;
-  fs.writeFile('user.txt', data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-    process.exit();
-  });
+function askQuestion(num){
+  process.stdout.write(`${questions[num]}\n`);
+}
+
+
+process.stdin.on('data', function (answer){
+  const inputAnswer = answer.toString().trim();
+
+  if (validUserName === false) {
+    userName = inputAnswer;
+    const fileName = inputAnswer.replace(/\s/g,'').toLowerCase();
+    if (fs.existsSync(`./results/${fileName}.txt`)) {
+      process.stdout.write(`\nSorry, someone with the name ${userName} has already completed the quiz, please enter another name (full name, nickname, etc)\n`);
+    } else {
+      const data = `Quiz Results for ${inputAnswer}`;
+      fs.writeFile(`./results/${fileName}.txt`, data, (err) => {
+        if (err) throw err;
+        validUserName = true;
+        process.stdout.write(`\nThank you ${userName} lets start the quiz.\n`);
+        askQuestion(0);
+    })
+   }
+  } else {
+    // This is where we will run the quiz
+    console.log(inputAnswer);
+  }
+
 });
+//
+// process.stdin.on('data',(answer) => {
+//   let inputAnswer = answer.toString().trim();
+//   const name = `User name is ${inputAnswer}`;
+//   console.log(answer);
+//   fs.writeFile('user.txt',(err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+//     process.exit();
+//   });
+// });
+
 
 askName();
